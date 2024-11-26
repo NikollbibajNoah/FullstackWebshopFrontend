c8750e74eeaf055129fa6c5766d2e6200c54c957
NikollbibajNoah <nikollbibaj.noah@gmail.com>
2024-11-26 17:51:45 +0100

    ant design init

---
+++ src/layout/MainLayout.tsx
@@ -0,0 +1,10 @@
+import { Button } from "antd";
+
+export default function MainLayout() {
+  return (
+    <>
+      <h1>Mainlayout</h1>
+      <Button type="primary">Hello!</Button>
+    </>
+  );
+}
